ccd4ef57f42ff4302918fadfa91d519e8ac191f6
Dr. Vortex <jp@drvortex.dev>
2024-05-04 14:13:56 -0500

    Streamlined range.ts

--- src/range.ts
+++ src/range.ts
@@ -1,141 +1,168 @@
 import { LineColumn, Source } from './source.js';
-import { StringIterator, string_fromCodePoints } from './unicode.js';
 
-export class FormattedRange {
+const iterator = {
+	value: '',
+	index: 0,
+	stop: 0,
+};
+
+function nextCodePoint(): number {
+	if (iterator.index >= iterator.stop) {
+		return -1;
+	}
+
+	iterator.index++;
+	const a = iterator.value.charCodeAt(iterator.index - 1);
+
+	if ((a & 64512) !== 55296) {
+		return a;
+	}
+
+	if (iterator.index >= iterator.stop) {
+		return -1;
+	}
+
+	iterator.index++;
+	const b = iterator.value.charCodeAt(iterator.index - 1);
+	return (a << 10) + b + (65536 - (55296 << 10) - 56320);
+}
+
+function fromCodePoints(codePoints: number[]): string {
+	let string = '';
+
+	for (const codePoint of codePoints) {
+		string += fromCodePoint(codePoint);
+	}
+
+	return string;
+}
+
+function fromCodePoint(codePoint: number): string {
+	return codePoint < 65536
+		? String.fromCharCode(codePoint)
+		: String.fromCharCode(((codePoint - 65536) >> 10) + 55296) + String.fromCharCode(((codePoint - 65536) & ((1 << 10) - 1)) + 56320);
+}
+
+export interface FormattedRange {
 	line: string;
 	range: string;
-
-	constructor(line: string, range: string) {
-		this.line = line;
-		this.range = range;
-	}
 }
 
 export class Range {
-	source: Source;
-	start: number;
-	end: number;
+	public constructor(
+		public source: Source,
+		public start: number,
+		public end: number
+	) {}
 
-	toString(): string {
+	public toString(): string {
 		return this.source.contents.slice(this.start, this.end);
 	}
 
-	locationString(): string {
+	public locationString(): string {
 		const location = this.source.indexToLineColumn(this.start);
 		return `${this.source.name}:${location.line + 1}:${location.column + 1}`;
 	}
 
-	overlaps(range: Range): boolean {
+	public overlaps(range: Range): boolean {
 		return this.source === range.source && this.start < range.end && range.start < this.end;
 	}
 
-	touches(index: number): boolean {
+	public touches(index: number): boolean {
 		return this.start <= index && index <= this.end;
 	}
 
-	format(maxLength: number): FormattedRange {
+	public format(maxLength: number): FormattedRange {
 		console.assert(this.source !== null);
 		const start = this.source.indexToLineColumn(this.start);
 		const end = this.source.indexToLineColumn(this.end);
 		let line = this.source.contentsOfLine(start.line);
-		const startColumn = start.column;
 		const endColumn = end.line === start.line ? end.column : line.length;
 
 		// Use a unicode iterator to count the actual code points so they don't get sliced through the middle
-		const iterator = StringIterator.INSTANCE.reset(line, 0);
+		iterator.value = line;
+		iterator.index = 0;
 		const codePoints: number[] = [];
 		let a = 0;
 		let b = 0;
+		let codePoint: number;
 
 		// Expand tabs into spaces
-		while (true) {
-			if (iterator.index === startColumn) {
+		while (codePoint >= 0) {
+			codePoint = nextCodePoint();
+
+			if (codePoint === 9) {
+				for (let space = 0; space < 8 - (codePoints.length % 8); space++) {
+					codePoints.push(32);
+				}
+			} else {
+				codePoints.push(codePoint);
+			}
+
+			if (iterator.index === start.column) {
 				a = codePoints.length;
 			}
 
 			if (iterator.index === endColumn) {
 				b = codePoints.length;
 			}
-
-			const codePoint = iterator.nextCodePoint();
-
-			if (codePoint < 0) {
-				break;
-			}
-
-			if (codePoint === 9) {
-				for (let space = 0, count1 = 8 - (codePoints.length % 8); space < count1; space = space + 1) {
-					codePoints.push(32);
-				}
-			} else {
-				codePoints.push(codePoint);
-			}
 		}
 
-		// Ensure the line length doesn't exceed maxLength
-		const count = codePoints.length;
-
-		if (maxLength > 0 && count > maxLength) {
-			const centeredWidth = Math.min(b - a, (maxLength / 2) | 0);
-			const centeredStart = Math.max(((maxLength - centeredWidth) / 2) | 0, 3);
-
-			// Left aligned
-			if (a < centeredStart) {
-				line = string_fromCodePoints(codePoints.slice(0, maxLength - 3)) + '...';
-
-				if (b > maxLength - 3) {
-					b = maxLength - 3;
-				}
-			}
-
-			// Right aligned
-			else if (count - a < maxLength - centeredStart) {
-				const offset = count - maxLength;
-				line = '...' + string_fromCodePoints(codePoints.slice(offset + 3, count));
-				a = a - offset;
-				b = b - offset;
-			}
-
-			// Center aligned
-			else {
-				const offset1 = a - centeredStart;
-				line = '...' + string_fromCodePoints(codePoints.slice(offset1 + 3, offset1 + maxLength - 3)) + '...';
-				a = a - offset1;
-				b = b - offset1;
-
-				if (b > maxLength - 3) {
-					b = maxLength - 3;
-				}
-			}
-		} else {
-			line = string_fromCodePoints(codePoints);
+		if (maxLength <= 0 || maxLength > codePoints.length) {
+			return {
+				line: fromCodePoints(codePoints),
+				range: ' '.repeat(a) + (b - a < 2 ? '^' : '~'.repeat(b - a)),
+			};
 		}
 
-		return new FormattedRange(line, ' '.repeat(a) + (b - a < 2 ? '^' : '~'.repeat(b - a)));
+		const centeredWidth = Math.min(b - a, (maxLength / 2) | 0);
+		const centeredStart = Math.max(((maxLength - centeredWidth) / 2) | 0, 3);
+
+		// Left aligned
+		if (a < centeredStart) {
+			line = fromCodePoints(codePoints.slice(0, maxLength - 3)) + '...';
+
+			b = Math.min(b, maxLength - 3);
+		}
+
+		// Right aligned
+		else if (codePoints.length - a < maxLength - centeredStart) {
+			const offset = codePoints.length - maxLength;
+			line = '...' + fromCodePoints(codePoints.slice(offset + 3, codePoints.length));
+			a -= offset;
+			b -= offset;
+		}
+
+		// Center aligned
+		else {
+			const offset = a - centeredStart;
+			line = '...' + fromCodePoints(codePoints.slice(offset + 3, offset + maxLength - 3)) + '...';
+			a -= offset;
+			b = Math.min(b - offset, maxLength - 3);
+		}
+
+		return {
+			line,
+			range: ' '.repeat(a) + (b - a < 2 ? '^' : '~'.repeat(b - a)),
+		};
 	}
 
-	slice(offsetStart: number, offsetEnd: number): Range {
-		console.assert(offsetStart >= 0 && offsetStart <= offsetEnd && offsetEnd <= this.end - this.start);
-		return new Range(this.source, this.start + offsetStart, this.start + offsetEnd);
+	public slice(start: number, end: number): Range {
+		console.assert(start >= 0 && start <= end && end <= this.end - this.start);
+		return new Range(this.source, this.start + start, this.start + end);
 	}
 
-	lineColumn(): LineColumn {
+	public lineColumn(): LineColumn {
 		return this.source.indexToLineColumn(this.start);
 	}
 
-	rangeAtEnd(): Range {
+	public rangeAtEnd(): Range {
 		return new Range(this.source, this.end, this.end);
 	}
 
-	static span(start: Range, end: Range): Range {
+	public static span(start: Range, end: Range): Range {
 		console.assert(start.source === end.source);
 		console.assert(start.start <= end.end);
 		return new Range(start.source, start.start, end.end);
-	}
-
-	constructor(source: Source, start: number, end: number) {
-		this.source = source;
-		this.start = start;
-		this.end = end;
 	}
 }
